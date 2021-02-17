4b22f20cd559b317d8fef92115278c8640abd85c
harry4 <harry@boostcommerce.net>
2021-02-17 12:21:45 +0700

    update-functions

--- functions/main.ts
+++ functions/main.ts
@@ -7,7 +7,7 @@ import { Parser } from 'json2csv';
 const convertJSONtoCSV = async (path: string, json: Record<string, any>, fields: Array<string>) => {
   const json2csvParser = new Parser({ fields, delimiter: ';' });
   const csv = json2csvParser.parse(json);
-  await fs.writeFileAsync(path, csv);
+  await fs.writeFileSync(path, csv);
 };
 
 const fetch = (url: string) => {
