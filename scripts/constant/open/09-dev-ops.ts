d5fef8b544ce9f71e0ee5785595b4d6d5d9a6885
harry4 <harry@boostcommerce.net>
2021-02-27 20:42:02 +0700

    add-dev-ops

--- scripts/constant/open/09-dev-ops.ts
+++ scripts/constant/open/09-dev-ops.ts
@@ -3,7 +3,15 @@
 export default [
   { step: 9, group: 'devops', personal: true, name: 'docker', repo: 'jenkinsci/docker' },
   { step: 9, group: 'devops', personal: true, name: 'git', repo: 'git/git' },
-  { step: 9, group: 'devops', personal: true, name: 'visual-studio-code', repo: 'microsoft/vscode' },
+  {
+    step: 9,
+    group: 'devops',
+    personal: true,
+    name: 'visual-studio-code',
+    repo: 'microsoft/vscode'
+  },
   { step: 9, group: 'devops', personal: true, name: 'docker-compose', repo: 'docker/compose' },
-  { step: 9, group: 'devops', personal: false, name: 'apache kafka', repo: 'apache/kafka' }
+  { step: 9, group: 'devops', personal: false, name: 'apache kafka', repo: 'apache/kafka' },
+  { step: 9, group: 'devops', personal: false, name: 'terraform', repo: 'hashicorp/terraform' },
+  { step: 9, group: 'devops', personal: false, name: 'vagrant', repo: 'hashicorp/vagrant' }
 ];
